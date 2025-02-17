889f3a9af898a93dab169cbf842773e9a84f561e
sub-arctic <subarctic@duck.com>
2025-02-18 00:55:46 +1100

    added setters and getters for Vector2D

--- physics.py
+++ physics.py
@@ -29,29 +29,45 @@ def compute_polygon_inertia(vertices, mass):
 
 class Vector2D:
     def __init__(self, x=0.0, y=0.0):
-        self.x = x
-        self.y = y
+        self._x = x
+        self._y = y
+
+    @property
+    def x(self):
+        return self._x
+
+    @property
+    def y(self):
+        return self._y
+
+    @x.setter
+    def x(self, new_x):
+        self._x = new_x
+
+    @y.setter
+    def y(self, new_y):
+        self._y = new_y
 
     def __add__(self, other):
-        return Vector2D(self.x + other.x, self.y + other.y)
+        return Vector2D(self._x + other.x, self._y + other.y)
 
     def __sub__(self, other):
-        return Vector2D(self.x - other.x, self.y - other.y)
+        return Vector2D(self._x - other.x, self._y - other.y)
 
     def __mul__(self, scalar):
-        return Vector2D(self.x * scalar, self.y * scalar)
+        return Vector2D(self._x * scalar, self._y * scalar)
 
     def __truediv__(self, scalar):
-        return Vector2D(self.x / scalar, self.y / scalar)
+        return Vector2D(self._x / scalar, self._y / scalar)
 
     def dot(self, other):
-        return self.x * other.x + self.y * other.y
+        return self._x * other.x + self._y * other.y
 
     def cross(self, other):
-        return self.x * other.y - self.y * other.x
+        return self._x * other.y - self._y * other.x
 
     def magnitude(self):
-        return math.hypot(self.x, self.y)
+        return math.hypot(self._x, self._y)
 
     def normalized(self):
         mag = self.magnitude()
@@ -60,19 +76,19 @@ class Vector2D:
     def rotated(self, angle):
         # rotate by angle using basic trig functions
         cos_a, sin_a = math.cos(angle), math.sin(angle)
-        return Vector2D(self.x * cos_a - self.y * sin_a,
-                        self.x * sin_a + self.y * cos_a)
+        return Vector2D(self._x * cos_a - self._y * sin_a,
+                        self._x * sin_a + self._y * cos_a)
 
     def perp(self):
         # get a perpendicular vector
-        return Vector2D(-self.y, self.x)
+        return Vector2D(-self._y, self._x)
 
-    def __getitem__(self, index):
-        if index == 0:
-            return self.x
-        if index == 1:
-            return self.y
-        raise IndexError("index out of range for vector2d")
+    # def __getitem__(self, index):
+    #     if index == 0:
+    #         return self.x
+    #     if index == 1:
+    #         return self.y
+    #     raise IndexError("index out of range for vector2d")
 
 class PhysicsEngine:
     def __init__(self, gravity=9.81):
